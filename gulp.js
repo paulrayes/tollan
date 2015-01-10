9a2e769ee776f0589f47623236c4408d95f8108b
Paul Rayes <pjoerayes@live.com>
2015-01-10 00:39:34 -0700

    Better Gulp integrated, additional app config settings, vendor.js is back
    - Integrated Gulp and nodemon much better, there’s now a single process and application code is only three lines
    - Added some config settings the application can control for things like deferred front-end loading, port, etc
    - Got separate vendor.js back to speed up builds, the new setting for whether to defer loading JS allows us to always have that false when we're in dev mode with a vendor.js so it actually works

---
+++ gulp.js
@@ -0,0 +1,201 @@
+var gulp = require('gulp');
+var source = require('vinyl-source-stream'); // Used to stream bundle for further handling
+var browserify = require('browserify');
+var watchify = require('watchify');
+var reactify = require('reactify');
+var gulpif = require('gulp-if');
+var uglify = require('gulp-uglify');
+var streamify = require('gulp-streamify');
+var less = require('gulp-less');
+var cleancss = new (require("less-plugin-clean-css"))({advanced: true});
+var plumber = require('gulp-plumber');
+var nodemon = require('gulp-nodemon');
+
+// External dependencies you do not want to rebundle while developing,
+// but include in your application deployment
+var dependencies = [
+	'react',
+	'react-router'
+];
+// Source files
+var src = './lib/browser.js';
+var css = './lib/styles/*.{less,css}';
+var assets = './lib/assets/*.*';
+// Destination folder
+var dest = './build/';
+
+var debug = (process.env.NODE_ENV === 'development');
+var sourceMapping = false;
+
+var TASK_COUNT = 4;
+var tasksCompleted = 0;
+
+var initialStart = Date.now();
+
+/**
+ * Compiles all our code into main.js
+ */
+var browserifyTask = function() {
+	var bundler = browserify({
+		extensions: ['.jsx'],
+		entries: [src], // Only need initial file, browserify finds the deps
+		transform: [reactify],
+		debug: sourceMapping, // Gives us sourcemapping
+		cache: {}, packageCache: {}, fullPaths: debug // Requirement of watchify
+	});
+
+	if (debug) {
+		// Do not include external libraries
+		dependencies.forEach(function(dep) {
+			bundler.external(dep);
+		});
+
+		// Watchify doesn't work in dist because we turned off fullPaths
+		var bundler  = watchify(bundler);
+	}
+
+	var rebundle = function() {
+		var updateStart = Date.now();
+		// Create new bundle that uses the cache for high performance
+		var stream = bundler.bundle()
+			.pipe(plumber()) // Fix error handling
+			.pipe(source('main.js'))
+			// Uglify in dist env
+			.pipe(gulpif(!debug, streamify(uglify())))
+			// Write the output
+			.pipe(gulp.dest(dest))
+			.on('end', function() {
+				console.log('Updated main.js in ' + (Date.now() - updateStart) + 'ms');
+				nodemonTask();
+			});
+	}
+
+	bundler.on('update', rebundle);
+
+	rebundle();
+};
+
+/**
+ * Compiles listed dependencies into vendor.js
+ * No watch as these will not typically change
+ * This task takes a long time as there's a lot of code here
+ * There's nothing we can do to speed it up, but at least it only runs once
+ */
+var vendorTask = function() {
+	var bundler = browserify({
+		debug: sourceMapping, // Gives us sourcemapping
+		require: dependencies
+	});
+
+	var rebundle = function() {
+		var updateStart = Date.now();
+		// Create new bundle that uses the cache for high performance
+		bundler.bundle()
+			.pipe(plumber()) // Fix error handling
+			.pipe(source('vendor.js'))
+			// Write the output
+			.pipe(gulp.dest(dest))
+			.on('end', function() {
+				console.log('Updated vendor.js in ' + (Date.now() - updateStart) + 'ms');
+				nodemonTask();
+			});
+
+	}
+
+	rebundle();
+};
+
+/**
+ * Compiles all our CSS into main.css
+ */
+var lessTask = function() {
+
+	var rebundle = function() {
+		var updateStart = Date.now();
+		if (debug) {
+			gulp.src(css)
+				.pipe(plumber()) // Fix error handling
+				// No uglify in dev env
+				.pipe(less({
+				}))
+				.pipe(gulp.dest(dest))
+				.on('end', function() {
+					console.log('Updated styles.css in ' + (Date.now() - updateStart) + 'ms');
+					nodemonTask();
+				});
+		} else {
+			gulp.src(css)
+				.pipe(plumber()) // Fix error handling
+				// Uglify in dist env
+				.pipe(less({
+					plugins: [cleancss]
+				}))
+				.pipe(gulp.dest(dest))
+				.on('end', function() {
+					console.log('Updated styles.css in ' + (Date.now() - updateStart) + 'ms');
+					nodemonTask();
+				});
+		}
+	};
+
+	rebundle();
+
+	if (debug) {
+		gulp.watch(css, rebundle);
+	}
+};
+
+/**
+ * Compiles all our CSS into main.css
+ */
+var assetsTask = function() {
+
+	var rebundle = function() {
+		var updateStart = Date.now();
+		gulp.src(assets)
+			.pipe(gulp.dest(dest))
+			.on('end', function() {
+				console.log('Updated assets in ' + (Date.now() - updateStart) + 'ms');
+				nodemonTask();
+			});
+	};
+
+	rebundle();
+
+	if (debug) {
+		gulp.watch(assets, rebundle);
+	}
+};
+
+var nodemonTask = function() {
+	if (tasksCompleted < TASK_COUNT) {
+		tasksCompleted++;
+		//console.log('Completed', tasksCompleted, 'of', TASK_COUNT, 'tasks.');
+	}
+	if (tasksCompleted == TASK_COUNT) {
+		console.log('Completed', tasksCompleted, 'tasks in', (Date.now() - initialStart) + 'ms');
+		nodemon({
+			ext: 'js,jsx,dot',
+			watch: ['*.*', 'node_modules/tollan']
+		})
+		.on('restart', function(file) {
+			console.log(file);
+		});
+	}
+};
+
+// Finally create the gulp tasks
+
+var task = function() {
+	require('./lib/copyright')();
+	console.log('Building front-end files for ' + process.env.NODE_ENV + ' environment.');
+	browserifyTask();
+	if (debug) {
+		vendorTask();
+	}
+	lessTask();
+	assetsTask();
+	//nodemonTask();
+}
+
+module.exports = task;
